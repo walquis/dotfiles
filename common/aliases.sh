8ca7db0f0fade3acbeb333139cba4cfb01c740c3
Chris Walquist <chris@walquist.net>
2023-10-11 16:49:08 -0500

    merge git aliases

--- common/aliases.sh
+++ common/aliases.sh
@@ -8,42 +8,11 @@ bindkey -v
 # Not really "common", but oh well...
 alias gv="/Applications/MacVim.app/Contents/MacOS/Vim -g"
 
+alias ll='ls -lrt'
 
 alias grip="grip --pass=$GRIP_GITHUB_TOKEN"
-alias adog="git log   --all --decorate --oneline --graph"
 
-# Include commit dates
-alias addog='git  log --all --decorate --oneline --graph --pretty=format:"%C(yellow)%h%Creset%C(red)%C(bold)%d%Creset %C(cyan)%cd%Creset %s" --date=format:"%y-%m%d-%H%M"'
-
-# Include login of committer
-alias adddog='git log --all --decorate --oneline --graph --pretty=format:"%C(yellow)%h%Creset %C(magenta)%cl%Creset%C(red)%C(bold)%d%Creset %C(cyan)%cd%Creset %s" --date=format:"%y-%m%d-%H%M"'
-
-alias gs="git status"
-alias gsm="git submodule"
-alias gsmu="git submodule update"
-alias gsms="git submodule status"
-alias gsma="git submodule add"
-alias gsmi="git submodule init"
-alias ga="git add"
-alias gb="git branch"
-alias gc="git commit"
-
-# Amend previous commit (--amend) to include everything in
-# working tree (-a) and staging area (commit), using the
-# same comment (--no-edit).
-alias guc='git commit -a --amend --no-edit'
-
-alias gco="git checkout"
-alias gcom="git checkout main"
-alias gd="git diff"
-alias gl="git config -l"
-alias gds='git diff --staged'
-alias gphm='git push heroku master'
-alias grv='git remote -v'
-alias grsu='git remote set-url --add'
-alias grso="git remote show origin"
-alias gp='git push'
-
+alias hs="hugo serve --cleanDestinationDir --disableFastRender"
 
 alias k="kubectl"
 alias kd="kubectl describe"
@@ -63,7 +32,7 @@ alias h="helm"
 
 alias b='bundle exec'
 alias c='code'
-alias grh='git rev-parse HEAD'
+
 alias l='ls -lrt'
 
 alias gril='grep -ril --exclude-dir=vendor --exclude-dir=.git --exclude-dir=node_modules --exclude-dir=venv --exclude-dir=_site'
@@ -87,11 +56,10 @@ alias pt=pytest
 
 alias pya=". venv/bin/activate"
 alias pyd="deactivate"
-export PYENV_ROOT="$HOME/.pyenv"  # This is the default
-# Add pyenv to path
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-#  install pyenv into the shell as a shell function; enable shims and autocompletion
-eval "$(pyenv init -)"
 
-# # Use pyenv-virtualenv to manage environments within pyenv
-# eval "$(pyenv virtualenv-init -)"
+
+alias vl="vi \`ls -lrt | perl -e '@t=<STDIN>; print STDOUT (\$t[\$#t] =~ m:(\S+)$:)[0]' \`"
+alias tl="tail -f \`ls -lrt | perl -e '@t=<STDIN>; print STDOUT (\$t[\$#t] =~ m:(\S+)$:)[0]' \`"
+
+source `dirname $0`/git-aliases.sh
+
---
+++ common/git-aliases.sh
@@ -0,0 +1,43 @@
+alias  dog='git log       --decorate --oneline --graph' # Git history, from current branch
+alias adog='git log --all --decorate --oneline --graph' # Git history
+alias gh2='git log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short' # Git history
+
+# Include commit dates
+alias addog='git log --all --decorate --oneline --graph --pretty=format:"%C(yellow)%h%Creset%C(cyan)%C(bold)%d%Creset %C(cyan)%cd%Creset %s" --date=format:"%y-%m%d-%H%M"' # Git history
+
+# Include login of committer
+alias adddog='git log --all --decorate --oneline --graph --pretty=format:"%C(yellow)%h%Creset %C(magenta)%cl%Creset%C(red)%C(bold)%d%Creset %C(cyan)%cd%Creset %s" --date=format:"%y-%m%d-%H%M"'
+
+alias gs="git status"
+alias gsm="git submodule"
+alias gsmu="git submodule update"
+alias gsms="git submodule status"
+alias gsma="git submodule add"
+alias gsmi="git submodule init"
+alias ga="git add"
+
+# Amend previous commit (--amend) to include everything in
+# working tree (-a) and staging area (commit), using the
+# same comment (--no-edit).
+alias guc='git commit -a --amend --no-edit'
+
+alias gl='git config -l'
+alias gds='git diff --staged'
+alias grv='git remote -v'
+alias grsu='git remote set-url --add'
+alias grso="git remote show origin"
+alias gp='git push'
+
+alias gc='git commit'
+alias gd='git diff'
+alias ggd='GIT_PAGER= git diff'
+alias ggds='GIT_PAGER= git diff --staged'
+alias gco='git checkout'
+alias gcom='git checkout main'
+alias gb='git branch'
+
+alias gril='grep -ril --exclude-dir=.git --exclude-dir=vendor --exclude-dir=public/assets --exclude-dir=node_modules --exclude-dir=Pods --exclude-dir=build'
+alias grh='git reset --hard'
+alias grm='git reset --mixed'
+alias grs='git reset --soft'
+alias grph='git rev-parse HEAD'
